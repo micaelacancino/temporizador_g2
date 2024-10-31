05cf41373f4d9eeb65fee022c2334661992b269c
micaela <cancinomicaelaanahi@gmail.com>
2024-10-31 14:18:41 -0700

    agregue array integrantes y estructura html de pagina nosotros

---
+++ componentes/js/nosotros.js
@@ -0,0 +1,26 @@
+const integrantes = [
+  {
+    nombrApellido: "Micaela Cancino",
+    descripcion: ".........",
+    imagen:
+      "https://media.licdn.com/dms/image/v2/D5603AQFg3XUO3gpmiw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714345608381?e=2147483647&v=beta&t=GSGD27Z5iadvwzE_BmklXtB5YGBBvSM1v0pWS0kHEEU",
+    gitHub: "https://github.com/micaelacancino",
+    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+  },
+  {
+    nombrApellido: "Ramiro Romano",
+    descripcion: ".........",
+    imagen:
+      "https://media.licdn.com/dms/image/v2/D5603AQFg3XUO3gpmiw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714345608381?e=2147483647&v=beta&t=GSGD27Z5iadvwzE_BmklXtB5YGBBvSM1v0pWS0kHEEU",
+    gitHub: "https://github.com/micaelacancino",
+    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+  },
+  {
+    nombrApellido: "Ignacio",
+    descripcion: ".........",
+    imagen:
+      "https://media.licdn.com/dms/image/v2/D5603AQFg3XUO3gpmiw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714345608381?e=2147483647&v=beta&t=GSGD27Z5iadvwzE_BmklXtB5YGBBvSM1v0pWS0kHEEU",
+    gitHub: "https://github.com/micaelacancino",
+    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+  },
+];
