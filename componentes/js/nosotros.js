e553d3eb2625f2d71aeeb26ca84402c2f74c1de2
micaela <cancinomicaelaanahi@gmail.com>
2024-11-02 18:50:35 -0700

    edite el valor de la propiedades en array de objetos

--- componentes/js/nosotros.js
+++ componentes/js/nosotros.js
@@ -6,25 +6,25 @@ const integrantes = [
     imagen:
       "https://media.licdn.com/dms/image/v2/D5603AQFg3XUO3gpmiw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714345608381?e=2147483647&v=beta&t=GSGD27Z5iadvwzE_BmklXtB5YGBBvSM1v0pWS0kHEEU",
     gitHub: "https://github.com/micaelacancino",
-    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+    linkedin: "https://www.linkedin.com/in/micaela-cancino",
   },
   {
     nombreApellido: "Ramiro Romano",
     descripcion:
-      " Hola! Mi nombre es Micaela, soy estudiante de ingenieria ensistemas de información, me gusta aprender idiomas y exploralas infinitas posibilidades que ofrece el mundo de la tecnología.",
+      " Hola! Mi nombre es Ramiro, soy estudiante de ingenieria ensistemas de información, me gusta aprender idiomas y exploralas infinitas posibilidades que ofrece el mundo de la tecnología.",
     imagen:
       "https://media.licdn.com/dms/image/v2/D4E03AQH8NPDXm9wAwA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1709913104394?e=2147483647&v=beta&t=71FLasRVGFgc5dawftxFa1fSds8D2uEhBT0ox_84kus",
-    gitHub: "https://github.com/micaelacancino",
-    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+    gitHub: "https://github.com/nahuel-11",
+    linkedin: "https://github.com/nahuel-11",
   },
   {
-    nombreApellido: "Ignacio Gonzalez",
+    nombreApellido: "Ignacio",
     descripcion:
-      " Hola! Mi nombre es Micaela, soy estudiante de ingenieria ensistemas de información, me gusta aprender idiomas y exploralas infinitas posibilidades que ofrece el mundo de la tecnología.",
+      " Hola! Mi nombre es Ignacio, soy estudiante de ingenieria ensistemas de información, me gusta aprender idiomas y exploralas infinitas posibilidades que ofrece el mundo de la tecnología.",
     imagen:
-      "https://media.licdn.com/dms/image/v2/D5603AQFg3XUO3gpmiw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1714345608381?e=2147483647&v=beta&t=GSGD27Z5iadvwzE_BmklXtB5YGBBvSM1v0pWS0kHEEU",
-    gitHub: "https://github.com/micaelacancino",
-    linkekin: "https://www.linkedin.com/in/micaela-cancino",
+      "https://el-pais.brightspotcdn.com/uploads/2020/12/09/5fd174e261901.png",
+    gitHub: "https://github.com/ignas11",
+    linkedin: "https://github.com/ignas11",
   },
 ];
 
@@ -35,12 +35,16 @@ function cards() {
     const cardHTML = `
       <div class="card cardUs">
        <div class="card-body">
-        <img src="${integrante.imagen}"   class="card-img-top" alt="foto-Micaela">
+        <img src="${integrante.imagen}"   class="card cardUs">
         <h3>${integrante.nombreApellido}</h3>
         <p>${integrante.descripcion}</p>
           <div class="text-center">
-        <a href="${integrante.gitHub}" target="_blank">GitHub</a>
-        <a href="${integrante.linkekin}" target="_blank">LinkedIn</a>
+        <a href="${integrante.gitHub}">
+                  <i class="bi bi-github fs-4 pe-3 text-dark"></i
+                ></a>
+         <a href="${integrante.linkedin}"
+                  ><i class="bi bi-linkedin fs-4 text-dark"></i
+                ></a>
       </div>
        </div>
     `;
@@ -49,12 +53,3 @@ function cards() {
 }
 
 cards();
-
-
-
-
-
-
-
-
-
