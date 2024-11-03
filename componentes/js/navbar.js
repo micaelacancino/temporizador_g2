ebb6c6fd80cfcb7f282697fa7acce4df6bb66f47
micaela <cancinomicaelaanahi@gmail.com>
2024-11-03 11:57:20 -0800

    actualice navbar

--- componentes/js/navbar.js
+++ componentes/js/navbar.js
@@ -1,42 +1,33 @@
-function navbar() {
-  const header = document.getElementById("header");
-  const nav = document.createElement("nav");
-  nav.classList.add("navbar navbar-expand-lg");
-  nav.innerHTML = `<div class="container-fluid">
-  <button
-    class="navbar-toggler"
-    type="button"
-    data-bs-toggle="collapse"
-    data-bs-target="#menu"
-    aria-controls="navbarSupportedContent"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="menu">
-    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-      <li class="nav-item jost">
-        <a class="nav-link font" aria-current="page" href="/index.html"
-          >Inicio</a
-        >
-      </li>
-
-      <li class="nav-item jost">
-        <a class="nav-link" href="#footer">Contacto</a>
-      </li>
-
-      <li class="nav-item jost">
-        <a class="nav-link" href="/componentes/pages/nosotros.html"
-          >Sobre nosotros</a
-        >
+const header = document.querySelector(".header");
+header.innerHTML = `<nav class= "navbar navbar-expand-lg fixed-top p-4"> 
+ <div class="container-fluid">
+   <button
+     class="navbar-toggler"
+     type="button"
+     data-bs-toggle="collapse"
+     data-bs-target="#menu"
+     aria-controls="navbarSupportedContent"
+     aria-expanded="false"
+     aria-label="Toggle navigation"
+   >
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse" id="menu">
+     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+       <li class="nav-item jost">
+         <a class="nav-link font" aria-current="page" href="/index.html"
+           >Inicio</a
+         >
+       </li>
+       <li class="nav-item jost">
+         <a class="nav-link" href="#footer">Contacto</a>
+       </li>
+       <li class="nav-item jost">
+         <a class="nav-link" href="/componentes/pages/nosotros.html"
+           >Sobre nosotros</a
+         >
       </li>
     </ul>
   </div>
-</div>`;
-
-  header[0].appendChild(nav);
-}
-
-export { navbar };
+ </div>
+ </nav> `;
