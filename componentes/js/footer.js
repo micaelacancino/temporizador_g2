be860cc83f157ef0bc049ffde96f00975a421fb2
nahuel <nahuelromano911@gmail.com>
2024-11-04 01:18:19 -0300

    se creo el estilo del error 404 y se vincularon los botones del nav y footer al error 404

--- componentes/js/footer.js
+++ componentes/js/footer.js
@@ -3,7 +3,7 @@ const footer = document.querySelector(".footer");
 footer.innerHTML = `<section class="container-fluid text-center">
         <article class="row my-4 my-md-2 pt-md-4">
           <div class="col-sm-12 col-md-6 mb-2 mb-md-0">    
-             <h4>Temporizador 🕑</h4>
+            <h4> <a href="/index.html">Temporizador 🕑</a></h4>
           </div>
           <div class="col-sm-12 col-md-6">
             <h5 class="jost">Soporte</h5>
@@ -11,14 +11,14 @@ footer.innerHTML = `<section class="container-fluid text-center">
               class="list-unstyled d-flex justify-content-center flex-column flex-md-row"
             >
               <li>
-                <a class="jost pe-2" href="#">Nosotros</a>
+                <a class="jost pe-2" href="/componentes/pages/nosotros.html">Nosotros</a>
               </li>
               <li>
-                <a class="jost px-2" href="$#">Contactános</a>
+                <a class="jost px-2" href="/componentes/pages/404.html">Contactános</a>
               </li>
 
               <li>
-                <a class="jost ps-2 ps-md-0" href="#">Ayuda</a>
+                <a class="jost ps-2 ps-md-0" href="/componentes/pages/404.html">Ayuda</a>
               </li>
             </ul>
           </div>
